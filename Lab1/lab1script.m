7d16c68bff797a043b12f27aa73bccb30bf5200c
Mat Arnold <mat.arnold.93@gmail.com>
2022-02-09 21:04:17 -0800

    full calcs

--- Lab1/lab1script.m
+++ Lab1/lab1script.m
@@ -12,7 +12,7 @@ steel.finLength_in = 2.284;
 alum.initDiameter_in = 0.506;
 alum.finDiameter_in = 0.365;
 alum.initLength_in = 2.00;
-alum.finLength_in = 2.594;
+alum.finLength_in = 2.376;
 
 brass.initDiameter_in = 0.506;
 brass.finDiameter_in = 0.317;
@@ -22,30 +22,30 @@ brass.finLength_in = 2.594;
 % Test Results
 steel.yieldForce_lbs = 17500;
 steel.yieldElongation_percent = 0.85;
-steel.ruptureForce_lbs = 18750;
+steel.ruptureForce_lbs = 13000;
 steel.ultimateForce_lbs = 18750;
 steel.proportionalLimit_lbs = 15000;
-steel.proportionalElongation_percent = 0.3;
+steel.proportionalElongation_percent = 0.25;
 % from textbook
 steel.expectedYoungs_psi = 29e6;
 
 
 alum.yieldForce_lbs = 13500;
 alum.yieldElongation_percent = 0.5;
-alum.ruptureForce_lbs = 14750;
-alum.ultimateForce_lbs = 14750;
+alum.ruptureForce_lbs = 14000;
+alum.ultimateForce_lbs = 16000;
 alum.proportionalLimit_lbs = 12000;
-alum.proportionalElongation_percent = 0.5;
+alum.proportionalElongation_percent = 0.6;
 % from textbook
 alum.expectedYoungs_psi = 10.1e6;
 
 
-brass.yieldForce_lbs = 9900;
+brass.yieldForce_lbs = 9800;
 brass.yieldElongation_percent = 0.8;
-brass.ruptureForce_lbs = 11000;
-brass.ultimateForce_lbs = 11000;
-brass.proportionalLimit_lbs = 7000;
-brass.proportionalElongation_percent = 0.4;
+brass.ruptureForce_lbs = 9000;
+brass.ultimateForce_lbs = 11500;
+brass.proportionalLimit_lbs = 8000;
+brass.proportionalElongation_percent = 0.5;
 % from textbook
 brass.expectedYoungs_psi = 15e6;
 
@@ -76,14 +76,48 @@ for k = 1:n
 end
 
 % print results
-for k=1:n
-    matl = matList{k};
-    properties = fieldnames(material.(matl));
-    m = length(properties);
-    for i = 1:m
-        property = properties{i};
-        fprintf('The %s of %s is %0.3f \n', property, matl, material.(matl).(property))
-    end
-    fprintf('\n')
-end
+
+fprintf('\t\t \t\t Steel \t\t Aluminum \t Brass\n')
+fprintf('Initial Area [in^2]: \t\t %0.4f \t %0.4f \t %0.4f\n', material.steel.initArea_in2, material.alum.initArea_in2,material.brass.initArea_in2)
+fprintf('Final Area [in^2]: \t\t %0.4f \t %0.4f \t %0.4f\n', material.steel.finArea_in2, material.alum.finArea_in2,material.brass.finArea_in2)
+fprintf('Percent Area Reduction: \t %0.4f \t %0.4f \t %0.4f\n', material.steel.areaReduction_percent, material.alum.areaReduction_percent,material.brass.areaReduction_percent)
+fprintf('Yield Stress [ksi]: \t\t %0.4f \t %0.4f \t %0.4f\n', material.steel.yieldStress_psi/1e3, material.alum.yieldStress_psi/1e3,material.brass.yieldStress_psi/1e3)
+fprintf('PL Stress [ksi]: \t\t %0.4f \t %0.4f \t %0.4f\n', material.steel.proportionalStress_psi/1e3, material.alum.proportionalStress_psi/1e3,material.brass.proportionalStress_psi/1e3)
+fprintf('PL Strain: \t\t\t %0.4f \t %0.4f \t %0.4f\n', material.steel.proportionalStrain, material.alum.proportionalStrain,material.brass.proportionalStrain)
+fprintf('Youngs Modulus [ksi]: \t\t %0.4f \t %0.4f \t %0.4f\n', material.steel.youngsModulus_psi/1e3, material.alum.youngsModulus_psi/1e3,material.brass.youngsModulus_psi/1e3)
+fprintf('Youngs Percent Error: \t\t %0.4f \t %0.4f \t %0.4f\n', material.steel.youngsError_percent, material.alum.youngsError_percent,material.brass.youngsError_percent)
+fprintf('Tensile Strength [ksi]: \t %0.4f \t %0.4f \t %0.4f\n', material.steel.tensileStress_psi/1e3, material.alum.tensileStress_psi/1e3,material.brass.tensileStress_psi/1e3)
+fprintf('Rupture Strength [ksi]: \t %0.4f \t %0.4f \t %0.4f\n', material.steel.ruptureStress_psi/1e3, material.alum.ruptureStress_psi/1e3,material.brass.ruptureStress_psi/1e3)
+fprintf('T/R Percent Difference: \t %0.4f \t %0.4f \t %0.4f\n', material.steel.ruptureTensileDiff_percent, material.alum.ruptureTensileDiff_percent,material.brass.ruptureTensileDiff_percent)
+fprintf('Modulus Resilience: \t\t %0.4f \t %0.4f \t %0.4f\n', material.steel.modResilience, material.alum.modResilience,material.brass.modResilience)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
